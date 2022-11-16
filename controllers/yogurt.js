609e907f64690d9b279d541788069e36fbd0a58e
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-16 13:14:43 -0600

    made changes in yogurt.js

--- controllers/yogurt.js
+++ controllers/yogurt.js
@@ -38,10 +38,10 @@ exports.yogurt_create_post = async function(req, res) {
 }; 
  
 // Handle yogurt delete form on DELETE. 
-exports.costume_delete = async function(req, res) { 
+exports.yogurt_delete = async function(req, res) { 
     console.log("delete "  + req.params.id) 
     try { 
-        result = await Costume.findByIdAndDelete( req.params.id) 
+        result = await yogurt.findByIdAndDelete( req.params.id) 
         console.log("Removed " + result) 
         res.send(result) 
     } catch (err) { 
