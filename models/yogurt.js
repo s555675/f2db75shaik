345f51caf7900082393cc75f91db9d2f970b1ac5
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-29 17:23:56 -0600

    Done with Part 5

--- models/yogurt.js
+++ models/yogurt.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose"); 
 const yogurtSchema = mongoose.Schema({ 
-    yogurt_flavour: String, 
-    calories: Number, 
-    yogurt_size: String
+    yogurt_flavour: {
+        type: String,
+        required: [true, "Flavour cannot be empty"]
+    }, 
+    calories: {
+        type: Number,
+        min: 0,
+        max: 250,
+        required: [true, "Calories cannot be empty"]
+    }, 
+    yogurt_size: {
+        type: String,
+        required: [true, "Size cannot be empty"]
+    }
 });
  
 module.exports = mongoose.model("yogurt", yogurtSchema);
